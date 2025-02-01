bcb3e05cf3c51902b6eb088409ee0eb4dda4696f
shazaM! <shazamjaved912@gmail.com>
2025-02-02 01:40:15 +0500

    Progress 2

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    
+    private float health;
+    private float lerpTimer;
+
+    [Header("Health Bar")]
+    public float maxHealth = 100;
+    public float chipSpeed = 2f;
+    public Image frontHealthbar;
+    public Image backHealthbar;
+    [Header("Damage Overlay")]
+    public Image overlay;
+    public float duration;
+    public float fadeSpeed;
+
+    private float durationTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = Mathf.Clamp(health, 0, maxHealth);
+        UpdateHealthUI();
+
+        if(overlay.color.a > 0)
+        {
+            if (health < 30)
+                return;
+            durationTimer = Time.deltaTime;
+            if(durationTimer > duration)
+            {
+                float tempAlpha = overlay.color.a;
+                tempAlpha -= Time.deltaTime*fadeSpeed;
+                overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, tempAlpha); 
+            }
+        }
+    }
+
+    private void UpdateHealthUI()
+    {
+
+        Debug.Log(health);
+        float fillF = frontHealthbar.fillAmount;
+        float fillB = backHealthbar.fillAmount;
+        float hFracton = health / maxHealth;
+        if(fillB>hFracton)
+        {
+            frontHealthbar.fillAmount = hFracton;
+            backHealthbar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            backHealthbar.fillAmount=Mathf.Lerp(fillB,hFracton,percentComplete);
+        }
+
+        if(fillF <hFracton)
+        {
+            backHealthbar.color = Color.green;
+            backHealthbar.fillAmount = hFracton;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            frontHealthbar.fillAmount = Mathf.Lerp(fillF, backHealthbar.fillAmount,percentComplete);
+        }
+
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        lerpTimer = 0f;
+        durationTimer = 0f;
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 1);
+    }
+
+    public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
+        lerpTimer = 0f;
+    }
+}
